af5517eee9c19264e14900ef31b98af9f6cbe323
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-08 00:28:49 +0800

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:trackminder/screens/signup_screen.dart';
+import 'package:trackminder/utils/color_utils.dart';
+
+import '../utils/reusable_widgets.dart';
+import 'home_screen.dart';
+
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({super.key});
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("20BF55"),
+          hexStringToColor("01BAEF")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(children: <Widget>[
+              logoWidget("assets/images/sa-white.png"),
+              const SizedBox(
+                height: 30,
+              ),
+              reusableTextField(
+                  "Enter email", Icons.person_outline, false, _emailController),
+              const SizedBox(
+                height: 30,
+              ),
+              reusableTextField("Enter password", Icons.lock_outline, true,
+                  _passwordController),
+              const SizedBox(
+                height: 20,
+              ),
+              authButtons(context, true, () {
+                FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text)
+                    .then((value) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                }).onError((error, stackTrace) {
+                  print("Error ${error.toString()}");
+                });
+              }),
+              signUpOption(),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account? ",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
