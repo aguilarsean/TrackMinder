fd2c7ebd19b133e6e1a9caf1ab4fb577105bcfd7
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-08 00:30:23 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:trackminder/screens/login_screen.dart';
+import 'package:trackminder/screen/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
