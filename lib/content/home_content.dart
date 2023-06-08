13f7d51a9775f6d7d6fc335ba5de45f9a616bf56
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-08 21:13:40 +0800

    add function to retreive user data when logged in

--- lib/content/home_content.dart
+++ lib/content/home_content.dart
@@ -1,10 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  String welcomeMessage = 'Welcome User';
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() {
+    setState(() {
+      isLoading = true;
+    });
+
+    User? user = FirebaseAuth.instance.currentUser;
+    print(user);
+
+    if (user != null) {
+      // String email = user.email ?? 'No email available';
+      String idNumber = user.displayName ?? 'No id number';
+
+      setState(() {
+        welcomeMessage = 'Welcome $idNumber';
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        welcomeMessage = 'Welcome User';
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return Scaffold(
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : Text(
+                welcomeMessage,
+                style: const TextStyle(fontSize: 24),
+              ),
+      ),
     );
   }
 }
