1fe9af91c6572c2df846e4a5ea84e3986de90f5f
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-08 22:39:11 +0800

    added unfocus text fields feature

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,7 +5,7 @@ import 'package:trackminder/screens/signup_screen.dart';
 import 'package:trackminder/utils/color_utils.dart';
 
 import '../utils/reusable_widgets.dart';
-import 'home_screen.dart';
+import 'main_screen.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -18,57 +18,64 @@ class _LogInScreenState extends State<LogInScreen> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
+  void _unfocusTextFields() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          hexStringToColor("20BF55"),
-          hexStringToColor("01BAEF")
-        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-            child: Column(children: <Widget>[
-              logoWidget("assets/images/sa-white.png"),
-              const SizedBox(
-                height: 30,
-              ),
-              reusableTextField(
-                  "Enter email", Icons.person_outline, false, _emailController),
-              const SizedBox(
-                height: 30,
-              ),
-              reusableTextField("Enter password", Icons.lock_outline, true,
-                  _passwordController),
-              const SizedBox(
-                height: 5,
-              ),
-              forgetPassword(context),
-              const SizedBox(
-                height: 20,
-              ),
-              authButtons(context, "LOG IN", () {
-                FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: _emailController.text,
-                        password: _passwordController.text)
-                    .then((value) {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomeScreen()));
-                }).onError((error, stackTrace) {
-                  print("Error ${error.toString()}");
-                });
-              }),
-              signUpOption(),
-            ]),
+      body: GestureDetector(
+        onTap: _unfocusTextFields,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("20BF55"),
+            hexStringToColor("01BAEF")
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+              child: Column(children: <Widget>[
+                logoWidget("assets/images/sa-white.png"),
+                const SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter email", Icons.person_outline, false,
+                    _emailController),
+                const SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter password", Icons.lock_outline, true,
+                    _passwordController),
+                const SizedBox(
+                  height: 5,
+                ),
+                forgetPassword(context),
+                const SizedBox(
+                  height: 20,
+                ),
+                authButtons(context, "LOG IN", () {
+                  FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text)
+                      .then((value) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()));
+                  }).onError((error, stackTrace) {
+                    print("Error ${error.toString()}");
+                  });
+                }),
+                signUpOption(),
+              ]),
+            ),
           ),
         ),
       ),
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -22,6 +22,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
   CollectionReference usersCollection =
       FirebaseFirestore.instance.collection('users');
 
+  void _unfocusTextFields() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,110 +39,113 @@ class _SignUpScreenState extends State<SignUpScreen> {
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          hexStringToColor("20BF55"),
-          hexStringToColor("01BAEF")
-        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-            child: Column(children: <Widget>[
-              const SizedBox(
-                height: 30,
-              ),
-              reusableTextField(
-                  "Enter email", Icons.person_outline, false, _emailController),
-              const SizedBox(
-                height: 30,
-              ),
-              reusableTextField("Enter ID number", Icons.numbers_outlined,
-                  false, _idNumberController),
-              const SizedBox(
-                height: 30,
-              ),
-              reusableTextField("Enter password", Icons.lock_outline, true,
-                  _passwordController),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        isProfessor = true;
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: isProfessor
-                          ? Colors.greenAccent
-                          : Colors.white.withOpacity(0.3),
+      body: GestureDetector(
+        onTap: _unfocusTextFields,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("20BF55"),
+            hexStringToColor("01BAEF")
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+              child: Column(children: <Widget>[
+                const SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter email", Icons.person_outline, false,
+                    _emailController),
+                const SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter ID number", Icons.numbers_outlined,
+                    false, _idNumberController),
+                const SizedBox(
+                  height: 30,
+                ),
+                reusableTextField("Enter password", Icons.lock_outline, true,
+                    _passwordController),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          isProfessor = true;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: isProfessor
+                            ? Colors.greenAccent
+                            : Colors.white.withOpacity(0.3),
+                      ),
+                      icon: const Icon(
+                        Icons.school,
+                        color: Colors.white70,
+                      ),
+                      label: Text(
+                        'Professor',
+                        style: TextStyle(color: Colors.white.withOpacity(0.9)),
+                      ),
                     ),
-                    icon: const Icon(
-                      Icons.school,
-                      color: Colors.white70,
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          isProfessor = false;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: !isProfessor
+                            ? Colors.greenAccent
+                            : Colors.white.withOpacity(0.3),
+                      ),
+                      icon: const Icon(
+                        Icons.school_outlined,
+                        color: Colors.white70,
+                      ),
+                      label: Text(
+                        'Student',
+                        style: TextStyle(color: Colors.white.withOpacity(0.9)),
+                      ),
                     ),
-                    label: Text(
-                      'Professor',
-                      style: TextStyle(color: Colors.white.withOpacity(0.9)),
-                    ),
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        isProfessor = false;
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: !isProfessor
-                          ? Colors.greenAccent
-                          : Colors.white.withOpacity(0.3),
-                    ),
-                    icon: const Icon(
-                      Icons.school_outlined,
-                      color: Colors.white70,
-                    ),
-                    label: Text(
-                      'Student',
-                      style: TextStyle(color: Colors.white.withOpacity(0.9)),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              authButtons(context, "SIGN UP", () {
-                usersCollection.add({
-                  'email': _emailController.text,
-                  'idNumber': _idNumberController.text,
-                  'isProfessor': isProfessor,
-                }).then((docRef) {
-                  print("data added with ID: ${docRef.id}");
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                authButtons(context, "SIGN UP", () {
+                  usersCollection.add({
+                    'email': _emailController.text,
+                    'idNumber': _idNumberController.text,
+                    'isProfessor': isProfessor,
+                  }).then((docRef) {
+                    print("data added with ID: ${docRef.id}");
 
-                  FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: _emailController.text,
-                          password: _passwordController.text)
-                      .then((value) {
-                    value.user!.updateDisplayName(_idNumberController.text);
+                    FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                            email: _emailController.text,
+                            password: _passwordController.text)
+                        .then((value) {
+                      value.user!.updateDisplayName(_idNumberController.text);
 
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomeScreen()));
-                  }).onError((error, stackTrace) {
-                    print("Error ${error.toString()}");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomeScreen()));
+                    }).onError((error, stackTrace) {
+                      print("Error ${error.toString()}");
+                    });
                   });
-                });
-              }),
-            ]),
+                }),
+              ]),
+            ),
           ),
         ),
       ),
