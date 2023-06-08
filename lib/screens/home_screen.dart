102f9302dd09ca98461f92b903c4ebe5f6745f45
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-08 15:26:20 +0800

    added navbar and refactor classes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,26 +3,83 @@ import 'package:flutter/material.dart';
 import 'package:trackminder/screens/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeContent(),
+    IdkContent(),
+    ProfileContent(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const LogInScreen()));
-            });
-          },
-        ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: 'Idk',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Home'),
+    );
+  }
+}
+
+class IdkContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('IDK'),
+    );
+  }
+}
+
+class ProfileContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text("Logout"),
+        onPressed: () {
+          FirebaseAuth.instance.signOut().then((value) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LogInScreen()),
+            );
+          });
+        },
       ),
     );
   }
