9e8101ca8de649cfff8d85cca82524e4271cda44
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-08 15:47:26 +0800

    changed authButtons parameters

--- lib/utils/reusable_widgets.dart
+++ lib/utils/reusable_widgets.dart
@@ -35,7 +35,7 @@ TextField reusableTextField(String text, IconData icon, bool isPassword,
   );
 }
 
-Container authButtons(BuildContext context, bool isLogin, Function onTap) {
+Container authButtons(BuildContext context, String title, Function onTap) {
   return Container(
     width: MediaQuery.of(context).size.width,
     height: 50,
@@ -55,7 +55,7 @@ Container authButtons(BuildContext context, bool isLogin, Function onTap) {
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))),
       child: Text(
-        isLogin ? "LOG IN" : "SIGN UP",
+        title,
         style: const TextStyle(
             color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
       ),
