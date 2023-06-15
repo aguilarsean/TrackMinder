d5267d270ff066fae1f814988e0255fd4fcc9fb8
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-15 22:50:43 +0800

    add carousel, get started, buttons

---
+++ lib/content/get_started.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:trackminder/screens/main_screen.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({Key? key}) : super(key: key);
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+  int _totalPages = 3;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            PageView(
+              controller: _pageController,
+              onPageChanged: (int page) {
+                setState(() {
+                  _currentPage = page;
+                });
+              },
+              children: [
+                // Page 1
+                Container(
+                  color: Colors.white,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.calendar_today, size: 80, color: Colors.blue),
+                      SizedBox(height: 16),
+                      Text(
+                        'Welcome to the App!',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                // Page 2
+                Container(
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          _buildItem(Icons.image, 'Image 1'),
+                          const SizedBox(height: 32),
+                          _buildItem(Icons.image, 'Image 2'),
+                          const SizedBox(height: 32),
+                          _buildItem(Icons.image, 'Image 3'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                // Page 3
+                Container(
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.smart_screen,
+                          size: 80, color: Colors.blue),
+                      const SizedBox(height: 16),
+                      const Text(
+                        'Enjoy the App!',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 32),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainScreen(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(16),
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Text(
+                          'Go Back to MainScreen',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildPageIndicator(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(IconData icon, String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Icon(icon, size: 40),
+        const SizedBox(height: 8),
+        Text(text, style: const TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < _totalPages; i++) {
+      indicators.add(
+        i == _currentPage ? _indicator(true) : _indicator(false),
+      );
+    }
+    return indicators;
+  }
+
+  Widget _indicator(bool isActive) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      height: 8.0,
+      width: 8.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isActive ? Colors.blue : Colors.grey,
+      ),
+    );
+  }
+}
--- lib/content/home_content.dart
+++ lib/content/home_content.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: unused_field, avoid_unnecessary_containers
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'get_started.dart';
+import '../utils/colors_typography.dart';
 
 class HomeContent extends StatefulWidget {
   @override
@@ -10,75 +16,279 @@ class HomeContent extends StatefulWidget {
 
 class _HomeContentState extends State<HomeContent> {
   String welcomeMessage = 'Welcome User';
-  bool isLoading = false;
+  String greeting = '';
+
+  int _currentImageIndex = 0;
+  List<String> images = [
+    'assets/images/cpe_logo.png',
+    'assets/images/cpec_robomania_wide.jpg',
+    'assets/images/usc_gdsc_wide.jpg',
+  ];
 
   @override
   void initState() {
     super.initState();
     getUserData();
+    setGreeting();
   }
 
-  void getUserData() async {
-    setState(() {
-      isLoading = true;
-    });
+  void setGreeting() {
+    DateTime now = DateTime.now();
+    int hour = now.hour;
 
+    if (hour < 12) {
+      setState(() {
+        greeting = 'Good morning';
+      });
+    } else if (hour < 17) {
+      setState(() {
+        greeting = 'Good afternoon';
+      });
+    } else {
+      setState(() {
+        greeting = 'Good evening';
+      });
+    }
+  }
+
+  Future<void> getUserData() async {
     User? user = FirebaseAuth.instance.currentUser;
-    print(user);
 
     if (user != null) {
-      String uid = user.uid;
-      String idNumber = user.displayName ?? 'No id number';
-
-      DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(idNumber)
-          .get();
-
-      Map<String, dynamic>? data = docSnapshot.data() as Map<String, dynamic>?;
-      String profileName = data?['profileName'] ?? '';
-
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.setString('uid', uid);
-      await prefs.setString('idNumber', idNumber);
-      await prefs.setString('profileName', profileName);
-
-      if (mounted) {
-        setState(() {
-          welcomeMessage = 'Welcome $profileName';
-          isLoading = false;
-        });
-      }
+      await fetchUserDataFromFirestore(user.uid, user.displayName);
     } else {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      String? uid = prefs.getString('uid');
-      String? idNumber = prefs.getString('idNumber');
-      String? profileName = prefs.getString('profileName');
-
-      if (uid != null && idNumber != null && profileName != null) {
-        setState(() {
-          welcomeMessage = 'Welcome $profileName';
-          isLoading = false;
-        });
-      } else {
-        setState(() {
-          isLoading = false;
-        });
-      }
+      await fetchUserDataFromSharedPrefs();
     }
+  }
+
+  Future<void> fetchUserDataFromFirestore(String uid, String? idNumber) async {
+    DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(idNumber ?? '')
+        .get();
+
+    Map<String, dynamic>? data = docSnapshot.data() as Map<String, dynamic>?;
+
+    String profileName = data?['profileName'] ?? '';
+
+    await saveUserDataToSharedPrefs(uid, idNumber, profileName);
+
+    setState(() {
+      welcomeMessage = 'Welcome $profileName';
+    });
+  }
+
+  Future<void> fetchUserDataFromSharedPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String? uid = prefs.getString(SharedPrefsKeys.uid);
+    String? idNumber = prefs.getString(SharedPrefsKeys.idNumber);
+    String? profileName = prefs.getString(SharedPrefsKeys.profileName);
+
+    if (uid != null && idNumber != null && profileName != null) {
+      setState(() {
+        welcomeMessage = 'Welcome $profileName';
+      });
+    } else {
+      setState(() {
+        welcomeMessage = 'Welcome User';
+      });
+    }
+  }
+
+  Future<void> saveUserDataToSharedPrefs(
+      String uid, String? idNumber, String profileName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(SharedPrefsKeys.uid, uid);
+    await prefs.setString(SharedPrefsKeys.idNumber, idNumber ?? '');
+    await prefs.setString(SharedPrefsKeys.profileName, profileName);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: isLoading
-            ? const CircularProgressIndicator()
-            : Text(
-                welcomeMessage,
-                style: const TextStyle(fontSize: 24),
+      backgroundColor: AppColors.accentColor,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 16),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  welcomeMessage,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  greeting,
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.width * 1 / 2.35,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: CarouselSlider(
+                    items: images.map((image) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Image.asset(
+                            image,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                          );
+                        },
+                      );
+                    }).toList(),
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 7),
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _currentImageIndex = index;
+                        });
+                      },
+                      viewportFraction: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 1),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: images.map((image) {
+                  int index = images.indexOf(image);
+                  return Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 4.0,
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentImageIndex == index
+                          ? Colors.blueAccent
+                          : Colors.grey,
+                    ),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const GetStarted(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(16),
+                          backgroundColor: Colors.white.withOpacity(0.95),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.rocket, size: 40, color: Colors.green),
+                            SizedBox(height: 10),
+                            Text('Get Started',
+                                style: TextStyle(color: Colors.green)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: AspectRatio(
+                              aspectRatio: 1.0,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.all(16),
+                                  backgroundColor:
+                                      Colors.white.withOpacity(0.95),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: const Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.dashboard,
+                                        size: 40, color: Colors.green),
+                                    SizedBox(height: 10),
+                                    Text('Dashboard',
+                                        style: TextStyle(color: Colors.green)),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  'Coming Soon',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+class SharedPrefsKeys {
+  static const String uid = 'uid';
+  static const String idNumber = 'idNumber';
+  static const String profileName = 'profileName';
 }
