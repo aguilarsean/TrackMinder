1aa235bb8404e2771dfc3a70f482c75a76eb5c5d
Sean Aguilar <seanaguilar698@gmail.com>
2023-06-15 22:49:29 +0800

    add color util

---
+++ lib/utils/colors_typography.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromRGBO(0, 71, 186, 1);
+  static const Color secondaryColor = Color.fromRGBO(230, 237, 248, 1);
+  static const Color accentColor = Color.fromRGBO(243, 242, 242, 1);
+  static const Color textColor = Color.fromRGBO(38, 28, 38, 1);
+}
